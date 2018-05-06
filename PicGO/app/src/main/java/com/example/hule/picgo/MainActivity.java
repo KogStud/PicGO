486386eae7c8fbd9a7a9889265ee0f4f5fa5cb96
zeroflame1 <zeroflame1@hotmail.com>
2018-05-06 13:13:08 +0200

    realBlackScreen

--- PicGO/app/src/main/java/com/example/hule/picgo/MainActivity.java
+++ PicGO/app/src/main/java/com/example/hule/picgo/MainActivity.java
@@ -49,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 launchCamera(v);
+                result_photo.setImageResource(R.drawable.blackbackground);
                 result_photo.setVisibility(View.VISIBLE);
             }
         });
